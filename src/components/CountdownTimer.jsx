b8d7221899a061aaa31d37acfa3198e4dd955524
hasni-hamdani <hasni.m.hamdani@gmail.com>
2025-07-26 12:30:04 +0200

    Update landing page with new changes

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 
 const CountdownTimer = () => {
-    const targetDate = new Date('2025-07-25T14:00:00'); // ⏰ Fixed deadline
+    const targetDate = new Date('2025-07-28T12:00:00'); // ⏰ Fixed deadline
 
     const [timeLeft, setTimeLeft] = useState({});
 
